cc035a683235be3bed45324b4ad52003c08a8c53
Kuba Podkalicki <kuba.pod@gmail.com>
2020-11-26 12:20:56 +0100

    0.0.2

--- MDataUtilities/PacletInfo.m
+++ MDataUtilities/PacletInfo.m
@@ -1,6 +1,6 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "MDataUtilities", Version -> "0.0.1", 
+Paclet[Name -> "MDataUtilities", Version -> "0.0.2", 
  MathematicaVersion -> "11.0+", Description -> 
-  "Data wrangling and visualization utilities for Mathematica",
+  "Data wrangling and visualization utilities for Mathematica", 
  Creator -> "Kuba Podkalicki (kuba.pod@gmail.com)", 
  Extensions -> {{"Kernel", Context -> "MDataUtilities`"}}]
